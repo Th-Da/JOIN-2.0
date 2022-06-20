501389f23d60840c4044a17fa530ac5480dc328c
Tobias Fernkorn <fernsikos@gmail.com>
2022-06-20 20:37:11 +0200

    added animation

---
+++ scripts/backlog.js
@@ -0,0 +1,14 @@
+function openBacklogCardDetails() {
+    document.getElementById('card-details-container').classList.remove('d-none');
+    setTimeout(() => {
+     document.getElementById('card-details-container').classList.remove('fade-out')   
+    }, 50);
+}
+
+function closeBacklogCardDetails() {
+    document.getElementById('card-details-container').classList.add('fade-out');
+    setTimeout(() => {
+        document.getElementById('card-details-container').classList.add('d-none')   
+       }, 500);
+
+}
