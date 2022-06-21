f91b3efeca62b9504f619aa6165951f3693894bb
Tobias Fernkorn <fernsikos@gmail.com>
2022-06-21 10:50:19 +0200

    finished board design

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,20 @@
 function stopPropagation(event) {
     event.stopPropagation();
 }
+
+// Fades objekts in with removing d-none and adds opacity transition
+function openCardDetails() {
+    document.getElementById('card-details-container').classList.remove('d-none');
+    setTimeout(() => {
+     document.getElementById('card-details-container').classList.remove('fade-out')   
+    }, 50);
+}
+
+// Fades objekts out with opacity transition and adding d-none
+function closeCardDetails() {
+    document.getElementById('card-details-container').classList.add('fade-out');
+    setTimeout(() => {
+        document.getElementById('card-details-container').classList.add('d-none')   
+       }, 500);
+
+}
