b1a0982b5622d8d077ebff6ed1af174dd8c5b53c
Dima <dima@Dietrichs-MacBook-Air.fritz.box>
2022-06-22 20:29:48 +0200

    addTask.js bearbeitet

--- scripts/addTask.js
+++ scripts/addTask.js
@@ -7,18 +7,35 @@ function createNewTask() {
     let urgencys = document.getElementById('input-field-urgency').value;
     let date = new Date().toDateString();
 
+    if (titles, categorys, descriptions, urgencys == '') {
+        alert('Please fill out all fields');
+    } else {
+
+        pushTaskInArray(titles, dueDates, categorys, descriptions, urgencys, date);
+
+        clearInputFields();
+    }
+}
+
+
+// clear the input fields
+function clearInputFields() {
+    document.getElementById('input-field-title').value = '';
+    document.getElementById('input-field-date').value = '';
+    document.getElementById('input-field-category').value = '';
+    document.getElementById('input-field-description').value = '';
+    document.getElementById('input-field-urgency').value = '';
+}
+
+
+//push in taskToDos Array
+function pushTaskInArray(titles, dueDates, categorys, descriptions, urgencys, date) {
     tasksToDos[3]['title'].push(titles);
     tasksToDos[3]['description'].push(descriptions);
     tasksToDos[3]['dueDate'].push(dueDates);
     tasksToDos[3]['category'].push(categorys);
     tasksToDos[3]['urgency'].push(urgencys);
     tasksToDos[3]['createdDate'].push(date);
-
-    document.getElementById('input-field-title').value = '';
-    document.getElementById('input-field-date').value = '';
-    document.getElementById('input-field-category').value = '';
-    document.getElementById('input-field-description').value = '';
-    document.getElementById('input-field-urgency').value = '';
 }
 
 
